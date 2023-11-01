2d89b4faf6acbeb63b904bac60177880d34cbf0c
Baekmina <koyoa12@naver.com>
2023-11-01 22:43:10 +0900

    function & return

--- assets/scripts/myscript.js
+++ assets/scripts/myscript.js
@@ -1,11 +1,15 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-currentResult = ((currentResult + 10) * 3) / 2 - 1;
+function add(num1, num2) {
+  const result = num1 + num2;
+  // alert("The result is " + result);
+
+  return result;
+}
+
+currentResult = add(1, 2);
 
 let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
-// let errorMessage = "An error \n occurred!";
-// let errorMessage = "An error \\ occurred!";
 
-// outputResult(currentResult, '');
 outputResult(currentResult, calculationDescription);
