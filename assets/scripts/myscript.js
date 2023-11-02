5c29a8022078e7c18495351ac8f89a496ff052ba
Baekmina <koyoa12@naver.com>
2023-11-02 23:14:02 +0900

    Split code into functions

--- assets/scripts/myscript.js
+++ assets/scripts/myscript.js
@@ -1,12 +1,22 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-function add() {
-  currentResult = currentResult + parseInt(userInput.value);
-  // parseInt = 소숫점 자리가 없는 숫자 (정수)
-  // parseFloat = 소숫점 자리가 있는 숫자
+function getUserNumberInput() {
+  return parseInt(userInput.value);
+}
 
-  outputResult(currentResult, "");
+function add() {
+  // 반복된 코드를 함수 선언을 통해 줄일 것.
+  // const enterrNumber = parseInt(userInput.value);
+
+  const enterrNumber = getUserNumberInput();
+  const calcDescription = `${currentResult} + ${enterrNumber}`;
+  currentResult = currentResult + enterrNumber;
+  outputResult(currentResult, calcDescription);
+
+  // parseInt = 소숫점 자리가 없는 숫자 (정수) & +userInput.value
+  // parseFloat = 소숫점 자리가 있는 숫자
+  // toString = 숫자를 문자열로 변환 (잘 사용안함)
 }
 
 addBtn.addEventListener("click", add);
