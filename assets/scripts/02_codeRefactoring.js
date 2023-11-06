964616b960fdf6a382fb1654aa0b8cd380f5de1d
Baekmina <koyoa12@naver.com>
2023-11-06 22:06:31 +0900

    object code repactoring

--- assets/scripts/02_codeRefactoring.js
+++ assets/scripts/02_codeRefactoring.js
@@ -6,16 +6,24 @@ function getUserNumberInput() {
   return parseInt(userInput.value);
 }
 
+// Generates and writes calculation log
 function createAndWriteOutput(operator, resultBeforCalc, calcNumber) {
   const calcDescription = `${resultBeforCalc} ${operator} ${calcNumber}`;
   outputResult(currentResult, calcDescription);
 }
 
 function add() {
+  // 반복된 코드를 함수 선언을 통해 줄일 것.
+  // const enterrNumber = parseInt(userInput.value);
+
   const enterrNumber = getUserNumberInput();
   const initialResult = currentResult;
   currentResult = currentResult + enterrNumber;
   createAndWriteOutput("+", initialResult, enterrNumber);
+
+  // parseInt = 소숫점 자리가 없는 숫자 (정수) & +userInput.value
+  // parseFloat = 소숫점 자리가 있는 숫자
+  // toString = 숫자를 문자열로 변환 (잘 사용안함)
 }
 
 function subtract() {
--- assets/scripts/myscript.js
+++ assets/scripts/myscript.js
@@ -7,47 +7,62 @@ function getUserNumberInput() {
   return parseInt(userInput.value);
 }
 
+// Generates and writes calculation log
 function createAndWriteOutput(operator, resultBeforCalc, calcNumber) {
   const calcDescription = `${resultBeforCalc} ${operator} ${calcNumber}`;
   outputResult(currentResult, calcDescription);
 }
 
-function add() {
-  const enterrNumber = getUserNumberInput();
-  const initialResult = currentResult;
-  currentResult += enterrNumber;
-  createAndWriteOutput("+", initialResult, enterrNumber);
-
+function writeToLog(
+  operationIdentifier,
+  prevResult,
+  operationNumber,
+  newResult
+) {
   // object (데이터를 함께 묵어 그룹화하는데에 사용)
   const logEntry = {
-    operation: "ADD",
-    prevResult: initialResult,
-    number: enterrNumber,
-    result: currentResult,
+    operation: operationIdentifier,
+    prevResult: prevResult,
+    number: operationNumber,
+    result: newResult,
   };
   logEntries.push(logEntry);
   console.log(logEntries);
+
+  // 객체 프로퍼티(데이터)에 접근하기
+  // console.log(logEntry.operation);
+}
+
+function add() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult += enteredNumber;
+  createAndWriteOutput("+", initialResult, enteredNumber);
+  writeToLog("ADD", initialResult, enteredNumber, currentResult);
 }
 
 function subtract() {
-  const enterrNumber = getUserNumberInput();
+  const enteredNumber = getUserNumberInput();
   const initialResult = currentResult;
-  currentResult -= enterrNumber;
-  createAndWriteOutput("-", initialResult, enterrNumber);
+  currentResult -= enteredNumber;
+  createAndWriteOutput("-", initialResult, enteredNumber);
+  writeToLog("SUBTRACT", initialResult, enteredNumber, currentResult);
 }
 
 function multiply() {
-  const enterrNumber = getUserNumberInput();
+  const enteredNumber = getUserNumberInput();
   const initialResult = currentResult;
-  currentResult *= enterrNumber;
-  createAndWriteOutput("*", initialResult, enterrNumber);
+  currentResult *= enteredNumber;
+  createAndWriteOutput("*", initialResult, enteredNumber);
+  writeToLog("MULTIPLY", initialResult, enteredNumber, currentResult);
 }
 
 function divide() {
-  const enterrNumber = getUserNumberInput();
+  const enteredNumber = getUserNumberInput();
   const initialResult = currentResult;
-  currentResult /= enterrNumber;
-  createAndWriteOutput("/", initialResult, enterrNumber);
+  currentResult /= enteredNumber;
+  createAndWriteOutput("/", initialResult, enteredNumber);
+  writeToLog("DIVIDE", initialResult, enteredNumber, currentResult);
 }
 
 addBtn.addEventListener("click", add);
