048572df30d6ebb51866c7d7f25e51736d99b5c7
Baekmina <koyoa12@naver.com>
2023-11-06 22:22:45 +0900

    dataType undefined, null, NaN / typeof

---
+++ assets/scripts/06_dataType&typeof.js
@@ -0,0 +1,20 @@
+// undefined
+// 아무것도 없음을 나타내는 값
+// 별도의 데이터 유형이나 undefined를 직접 값으로 할당하면 안됨.
+// ex) let userName; userName = undefined;
+
+// null
+// undefined와 같이 아무것도 없음을 나타내는 값
+// null은 절대 기본값이 될 수 없음.
+// 해당 값을 사용하려면 무언가를 null로 설정해야함. (변수를 재설정하거나 정리하고 싶을 때 사용)
+// ex) let userName; userName = null
+// 나중에 값이 설정됐는지 확인하기 위해 if문을 사용할 때 매우 유용
+
+// NaN
+// 일종의 오류 코드. 숫자가 아닌 무언가로 연산한 경우 그 결과로 NaN이 나옴.
+// 유효하지 않은 계산을 한 경우 NaN값이 나옴.
+
+// typeof
+// 런타임에서 변수유형을 평가할 수 있게 해줌.
+// typeof 'Max'
+// "string"
