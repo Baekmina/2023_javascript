3dc2f9dd0c93b1f7d5ee5b9a3a47ce062fe41b1b
Baekmina <koyoa12@naver.com>
2023-11-28 22:27:34 +0900

    Implementing Code Game Logic

--- 04_rock-papar-scissors.js
+++ 04_rock-papar-scissors.js
@@ -4,9 +4,13 @@ const ROCK = "ROCK";
 const PAPER = "PAPER";
 const SCISSORS = "SCISSORS";
 const DEFAULT_USER_CHOICE = ROCK;
+const RESULT_DRAW = "DRAW";
+const RESULT_PLAYER_WINS = "PLAYER_WINS";
+const RESULT_COMPUTER_WINS = "COMPUTER_WINS";
 
 let gameIsRunning = false;
 
+// 플레이어가 무엇을 낼지 설정
 const getPlayerChoice = function () {
   const selection = prompt(
     `${ROCK}, ${PAPER} or ${SCISSORS}?`,
@@ -19,6 +23,34 @@ const getPlayerChoice = function () {
   return selection;
 };
 
+// 컴퓨터가 무엇을 낼지 설정
+const getComputerChoice = function () {
+  // Math = 0과 1사이의 난수를 생성하는 random 메서드 (브라우저를 통해 사용할 수 있는 전역 객체)
+  const randomValue = Math.random();
+  if (randomValue < 0.34) {
+    return ROCK;
+  } else if (randomValue < 0.67) {
+    return PAPER;
+  } else {
+    return SCISSORS;
+  }
+};
+
+// winner
+const getWinner = function (cChoice, pChoice) {
+  if (cChoice === pChoice) {
+    return RESULT_DRAW;
+  } else if (
+    (cChoice === ROCK && pChoice === PAPER) ||
+    (cChoice === PAPER && pChoice === SCISSORS) ||
+    (cChoice === SCISSORS && pChoice === ROCK)
+  ) {
+    return RESULT_PLAYER_WINS;
+  } else {
+    return RESULT_COMPUTER_WINS;
+  }
+};
+
 startGameBtn.addEventListener("click", function () {
   // 버튼을 계속 클릭해도 새로운 게임이 실행되지 않도록 반환
   if (gameIsRunning) {
@@ -26,6 +58,9 @@ startGameBtn.addEventListener("click", function () {
   }
   gameIsRunning = true;
   console.log("Game is starting...");
-  const playerSelection = getPlayerChoice();
-  console.log(playerSelection);
+  const playerChoice = getPlayerChoice();
+  // console.log(playerChoice);
+  const computerChoice = getComputerChoice();
+  const winner = getWinner(computerChoice, playerChoice);
+  console.log(winner);
 });
