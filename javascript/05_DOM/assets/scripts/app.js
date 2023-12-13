b148a46e7e0ad4d9b6be56b9a21ac6bf4d41efa8
Baekmina <koyoa12@naver.com>
2023-12-13 21:03:02 +0900

    Created modal & button acess method

---
+++ javascript/05_DOM/assets/scripts/app.js
@@ -0,0 +1,13 @@
+// acess method
+const addMovieModal = document.getElementById("add-modal");
+// const addMovieModal = document.querySelector("#add-modal"); //querySelector의 경우 선택자 필요
+// const addMovieModal = document.body.children[1]; //body의 두번째 요소
+
+const startAddMovieButton = document.querySelector("header button");
+
+// const addMovieBtn = document.getElementById("add-button");
+// const addModal = document.getElementById("add-modal");
+
+// addMovieBtn.addEventListener("click", () => {
+//   addModal();
+// });
