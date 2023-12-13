8ab1e7ac1e865f63d42f8ca6a3ffd9a9d891cb71
Baekmina <koyoa12@naver.com>
2023-12-13 21:45:44 +0900

    Added backdropClickHandler & CancelAddMovieButton

--- javascript/05_DOM/assets/scripts/app.js
+++ javascript/05_DOM/assets/scripts/app.js
@@ -4,19 +4,33 @@ const addMovieModal = document.getElementById("add-modal");
 // const addMovieModal = document.body.children[1]; //body의 두번째 요소
 
 // add button acess method
-// const startAddMovieButton = document.querySelector("header button");
-// const startAddMovieButton = document.querySelector("header").lastElementChild; // 구조에 변경이 생기면 작동X
 const startAddMovieButton = document.querySelector("header button");
+// const startAddMovieButton = document.querySelector("header").lastElementChild; // 구조에 변경이 생기면 작동X
+
+// baekdrop acess method
+const backdrop = document.getElementById("backdrop");
+// const backdrop = document.body.firstElementChild;
+
+// cancel
+const CancelAddMovieButton = addMovieModal.querySelector(".btn--passive");
+
+const toggleBackdrop = () => {
+  backdrop.classList.toggle("visible");
+};
 
 const toggleMovieModal = () => {
   addMovieModal.classList.toggle("visible");
+  toggleBackdrop();
+};
+
+const CancleAddMovie = () => {
+  toggleMovieModal();
+};
+
+const backdropClickHandler = () => {
+  toggleMovieModal();
 };
 
 startAddMovieButton.addEventListener("click", toggleMovieModal);
-
-// const addMovieBtn = document.getElementById("add-button");
-// const addModal = document.getElementById("add-modal");
-
-// addMovieBtn.addEventListener("click", () => {
-//   addModal();
-// });
+backdrop.addEventListener("click", backdropClickHandler);
+CancelAddMovieButton.addEventListener("click", CancleAddMovie);
