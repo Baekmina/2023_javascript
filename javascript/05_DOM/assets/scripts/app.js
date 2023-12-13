7f5e24de9e9240d81c06f1ae60004442f17a1011
Baekmina <koyoa12@naver.com>
2023-12-13 21:28:04 +0900

    Added modal classList toggle

--- javascript/05_DOM/assets/scripts/app.js
+++ javascript/05_DOM/assets/scripts/app.js
@@ -1,9 +1,18 @@
-// acess method
+// modal acess method
 const addMovieModal = document.getElementById("add-modal");
 // const addMovieModal = document.querySelector("#add-modal"); //querySelector의 경우 선택자 필요
 // const addMovieModal = document.body.children[1]; //body의 두번째 요소
 
+// add button acess method
+// const startAddMovieButton = document.querySelector("header button");
+// const startAddMovieButton = document.querySelector("header").lastElementChild; // 구조에 변경이 생기면 작동X
 const startAddMovieButton = document.querySelector("header button");
+
+const toggleMovieModal = () => {
+  addMovieModal.classList.toggle("visible");
+};
+
+startAddMovieButton.addEventListener("click", toggleMovieModal);
 
 // const addMovieBtn = document.getElementById("add-button");
 // const addModal = document.getElementById("add-modal");
